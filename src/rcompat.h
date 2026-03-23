7a29daf5aae034235e6b99d862c52021d3de03cf
Bruno Tremblay <bruno.tremblay@beneva.ca>
2026-03-23 16:53:18 -0400

    Use R_getExVar to avoid error print, no error print in findvar orig

--- src/rcompat.h
+++ src/rcompat.h
@@ -43,7 +43,7 @@
 #ifdef Rf_findVar
 #undef Rf_findVar
 #endif
-#define Rf_findVar(X, Y) R_getVar(Y, X, TRUE)
+#define Rf_findVar(X, Y) R_getExVar(Y, X, TRUE, R_UnboundValue)
 #ifndef findVar
 #define findVar Rf_findVar
 #endif
@@ -51,7 +51,7 @@
 #ifdef Rf_findVarInFrame
 #undef Rf_findVarInFrame
 #endif
-#define Rf_findVarInFrame(X, Y) R_getVar(Y, X, FALSE)
+#define Rf_findVarInFrame(X, Y) R_getExVar(Y, X, FALSE, R_UnboundValue)
 #ifndef findVarInFrame
 #define findVarInFrame Rf_findVarInFrame
 #endif
